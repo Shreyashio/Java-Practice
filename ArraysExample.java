c8cc3bee68070b4966e926f4631ea3c7f09ed251
Shreyash_Raut <rshreyash784@gmail.com>
2026-07-27 23:44:42 +0530

    Don't know what to write incommit message

--- ArraysExample.java
+++ ArraysExample.java
@@ -3,18 +3,24 @@ import java.util.*;
 public class ArraysExample{
     public static int getLargest(int numbers[]) {
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
         for(int i=0; i<numbers.length; i++) {
             if(largest<numbers[i]){
                 largest = numbers[i];
+            } 
+            if(smallest>numbers[i]){
+                smallest = numbers[i];
             }
         }
-        return largest;
+        // return largest;
+        return smallest;
     }
     public static void main (String args[]){
         int numbers[] = {1,2,5,3,7,6,8,9};
-        int largestNum = getLargest(numbers);
+        int smallestNum = getLargest(numbers);
 
-        System.out.println("The largest number in the array is : "+ largestNum);
+        System.out.println("The smallest number in the array is : "+ smallestNum);
 
 
     }
